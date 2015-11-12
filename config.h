174ae807c8b7b0d7400c168728142fa03998282a
张宏涛 <zhanghongtao@mioji.com>
2015-11-12 15:26:57 +0800

    init makefile

---
+++ config.h
@@ -0,0 +1,23 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "threadpool"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "htao_zhang@126.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ThreadPool"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ThreadPool 1.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "threadpool"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.0"
+
+/* Version number of package */
+#define VERSION "1.0.0"
